8fea965277bbdd5a486334597318e002b4430b84
Alexey Komarovskii <lopas1902@gmail.com>
2024-04-17 12:20:12 +0300

    Define layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="container mx-auto py-8">
+    <nav>
+      <ul>
+        <li><NuxtLink to="/index" class="header__link">Главная</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+  .header__link {
+    @apply text-2xl
+  }
+</style>
